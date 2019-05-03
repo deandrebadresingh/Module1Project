9f8aa9e8b4fa9c3b7e9a0acd068cdf0a75b58064
DeAndre <de.andre.badresingh@brooklynsteamcenter.org>
2019-05-03 13:06:31 -0400

    something

--- Module1Project/ViewController.swift
+++ Module1Project/ViewController.swift
@@ -9,11 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+   /* @IBAction func button2(_ sender: Any) {
+        view1.backgroundColor = UIColor.white
+    } */
+    
+    @IBOutlet weak var buttonOutlet: UIButton!
+    @IBOutlet var view1: UIView!
+    @IBAction func button1(_ sender: Any) {
+        if view1.backgroundColor == UIColor.black {
+            view1.backgroundColor = UIColor.white
+            buttonOutlet.setTitle("OFF", for: .normal)
+        } else {
+            view1.backgroundColor = UIColor.black
+            buttonOutlet.setTitle("ON", for: .normal)
+        }
+    
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+    } 
 
 
 }
